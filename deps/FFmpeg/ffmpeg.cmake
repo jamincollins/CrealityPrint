50fd4a195753f515f1f305ebcd72b873f402e784
Jamin W. Collins <jamin.collins@gmail.com>
2025-05-06 14:37:03 +0000

    update FFmpeg version
    fixes builds on Ubuntu 24.04
    
    Signed-off-by: Jamin W. Collins <jamin.collins@gmail.com>

--- deps/FFmpeg/ffmpeg.cmake
+++ deps/FFmpeg/ffmpeg.cmake
@@ -4,8 +4,8 @@ set(_make_cmd make -j${NPROC})
 set(_install_cmd make -j${NPROC} install)
 ExternalProject_Add(dep_FFmpeg
     #EXCLUDE_FROM_ALL ON
-    URL "https://ffmpeg.org/releases/ffmpeg-4.2.tar.bz2"
-    URL_HASH SHA256=306bde5f411e9ee04352d1d3de41bd3de986e42e2af2a4c44052dce1ada26fb8
+    URL "https://ffmpeg.org/releases/ffmpeg-4.4.5.tar.bz2"
+    URL_HASH SHA256=501c299145c3e86c74ea73046286eb8313d0d36b6b405c03cf26455799a47910
     DOWNLOAD_DIR ${DEP_DOWNLOAD_DIR}/FFmpeg
 	CONFIGURE_COMMAND ${_conf_cmd}
         "--prefix=${DESTDIR}"
